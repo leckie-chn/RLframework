1ad4ae11269960d366fb6a62d101cea0e55dc317
Yan Ni <niyan.ny@pku.edu.cn>
2017-03-07 16:55:39 +0800

    change critic network

--- Environment.py
+++ Environment.py
@@ -5,7 +5,6 @@ import numpy as np
 
 
 class Environment(object):
-
     def __init__(self):
         self.capacity = np.asarray([20.0, 30.0, 50.0])
         self.bufsize = np.asarray([10.0, 15.0, 20.0])
@@ -29,7 +28,7 @@ class Environment(object):
         drop_flow = np.sum(np.maximum(self.buffer - self.bufsize, np.zeros_like(self.buffer)))
         self.buffer = np.minimum(self.buffer, self.bufsize)
         load_factor = cur_flow / self.capacity
-	drop_penalty = 0.0 if self.flow_in == 0 else drop_flow / self.flow_in * 100
+        drop_penalty = 0.0 if self.flow_in == 0 else drop_flow / self.flow_in * 100
         reward = 1.0 - np.max(load_factor) - drop_penalty
         next_state = self.get_state()
         isTerminal = True if self.t > self.T * 5 * math.pi else False
