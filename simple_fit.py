8214c8d87e0e7810600419b1cc7c435b41fbaa8d
Yan Ni <niyan.ny@pku.edu.cn>
2017-03-17 12:04:05 +0800

    update simple_fit

--- simple_fit.py
+++ simple_fit.py
@@ -6,7 +6,7 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 
 model = Sequential()
-model.add(Dense(32, input_dim=2, activation='relu'))
+model.add(Dense(32, input_dim=1, activation='relu'))
 model.add(Dense(32, activation='softplus'))
 model.add(Dense(32, activation='softplus'))
 model.add(Dense(32, activation='relu'))
@@ -14,11 +14,11 @@ model.add(Dense(1, activation='linear'))
 
 model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error', metrics=['mean_squared_error'])
 
-eps = 1e-8  # prevent from division by 0
+scale = 2000.00
 num_data = 10000
 num_round = 5000
-data = np.random.random((num_data, 2)) + eps
-y = (data[:, 0] / data[:, 1]).reshape(num_data)
+data = np.random.random((num_data, 1)) * scale + 1.0
+y = 1.0 / data
 
 # print data
 # print y
@@ -26,5 +26,10 @@ y = (data[:, 0] / data[:, 1]).reshape(num_data)
 his = model.fit(data, y, nb_epoch=num_round, batch_size=32, verbose=0)
 plt.figure()
 plt.plot(np.log10(his.history['mean_squared_error']), 'b-')
-plt.show()
-plt.close()
+plt.savefig('logs/simple_loss.png')
+
+pred = model.predict_on_batch(data)[:, 0]
+plt.figure()
+plt.plot(np.arange(0.1, 0.1, 2000), 1.0 / np.arange(0.1, 0.1, 2000), 'b-')
+plt.plot(data, pred, 'ro')
+plt.savefig('logs/simple_fit.png')
