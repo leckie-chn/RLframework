11c2a7c90a84ba2d869fdb00b9f20b9ea4c1d396
Yan Ni <niyan.ny@pku.edu.cn>
2017-03-23 11:17:12 +0800

    sep training from test

--- simple_fit.py
+++ simple_fit.py
@@ -5,31 +5,36 @@ from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential
 from keras.optimizers import Adam
 
+mode = 'test'
+scale = 2.00
+num_data = 10000
+data = np.random.random((num_data, 1)) * scale + 1e-4
 model = Sequential()
 model.add(Dense(32, input_dim=1, activation='relu'))
-model.add(Dense(32, activation='softplus'))
-model.add(Dense(32, activation='softplus'))
+model.add(Dense(32, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='linear'))
-
 model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error', metrics=['mean_squared_error'])
 
-scale = 2000.00
-num_data = 10000
-num_round = 5000
-data = np.random.random((num_data, 1)) * scale + 1.0
-y = 1.0 / data
+if mode == 'training':
+    num_round = 20
+    y = 1.0 / data
+    his = model.fit(data, y, nb_epoch=num_round, batch_size=32, verbose=1)
 
-# print data
-# print y
+    plt.figure()
+    plt.plot(np.log10(his.history['mean_squared_error']), 'b-')
+    plt.savefig('logs/simple_loss.png')
 
-his = model.fit(data, y, nb_epoch=num_round, batch_size=32, verbose=0)
-plt.figure()
-plt.plot(np.log10(his.history['mean_squared_error']), 'b-')
-plt.savefig('logs/simple_loss.png')
+    model.save_weights('saved_networks/simple_fit.h5')
 
-pred = model.predict_on_batch(data)[:, 0]
-plt.figure()
-plt.plot(np.arange(0.1, 0.1, 2000), 1.0 / np.arange(0.1, 0.1, 2000), 'b-')
-plt.plot(data, pred, 'ro')
-plt.savefig('logs/simple_fit.png')
+
+elif mode == 'test':
+    model.load_weights('saved_networks/simple_fit.h5')
+
+    data = np.sort(data, axis=0)
+    pred = model.predict_on_batch(data)[:, 0]
+    plt.figure()
+    plt.plot(np.arange(1e-4, scale, 1e-4), 1.0 / np.arange(1e-4, scale, 1e-4), 'b-')
+    plt.plot(data, pred, 'ro')
+    plt.show()
+    plt.close()
