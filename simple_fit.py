17be4a97ab2505e192ebbdf459f83a2f97390c58
Yan Ni <niyan.ny@pku.edu.cn>
2017-03-23 11:42:38 +0800

    sep training from test

--- simple_fit.py
+++ simple_fit.py
@@ -7,8 +7,9 @@ from keras.optimizers import Adam
 
 mode = 'test'
 scale = 50.00
+base = 10.0
 num_data = 10000
-data = np.random.random((num_data, 1)) * scale + 10.0
+data = np.random.random((num_data, 1)) * scale + base
 model = Sequential()
 model.add(Dense(32, input_dim=1, activation='relu'))
 model.add(Dense(32, activation='relu'))
@@ -16,10 +17,10 @@ model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='linear'))
 model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error', metrics=['mean_squared_error'])
 
-if mode == 'training':
+if mode == 'train':
     num_round = 1000
     y = 1.0 / data
-    his = model.fit(data, y, nb_epoch=num_round, batch_size=32, verbose=1)
+    his = model.fit(data, y, nb_epoch=num_round, batch_size=32, verbose=0)
 
     plt.figure()
     plt.plot(np.log10(his.history['mean_squared_error']), 'b-')
@@ -34,7 +35,7 @@ elif mode == 'test':
     data = np.sort(data, axis=0)
     pred = model.predict_on_batch(data)[:, 0]
     plt.figure()
-    plt.plot(np.arange(1e-4, scale, 1e-4), 1.0 / np.arange(1e-4, scale, 1e-4), 'b-')
+    plt.plot(np.arange(base, scale, 1e-4), 1.0 / np.arange(base, scale, 1e-4), 'b-')
     plt.plot(data, pred, 'ro')
     plt.show()
     plt.close()
